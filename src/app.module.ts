fcbf51ef53038966c74051b85fbc9c46ab9de364
VeenaPD <veenapd4@gmail.com>
2025-06-01 19:45:07 +0530

    modules added with scalable architecture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductModule } from './product/product.module';
-import { CategoriesModule } from './categories/category.module';
-import { UserModule } from './user/user.module';
+import { ProductModule } from './modules/product/product.module';
+import { CategoriesModule } from './modules/categories/category.module';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { OrderModule } from './modules/order/order.module';
+import { CartModule } from './modules/cart/cart.module';
+import { PaymentModule } from './modules/payment/payment.module';
+import { ConfigModule } from './core/config/config/config.module';
+
 
 @Module({
-  imports: [ProductModule, CategoriesModule, UserModule],
+  imports: [ProductModule, CategoriesModule, UserModule, AuthModule, OrderModule, CartModule, PaymentModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
   exports: []
